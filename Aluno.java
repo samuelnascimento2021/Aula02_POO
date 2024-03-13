223a3e30e3d3f90e91d055cbf8df09d178073c82
Samuel Brayan <85649169+samuelnascimento2021@users.noreply.github.com>
2024-03-13 10:05:28 -0300

    Exercício 1

---
+++ Aluno.java
@@ -0,0 +1,33 @@
+public class Aluno {
+
+    String nome;
+    int matricula;
+    double nota_mat;
+    double nota_por;
+    double nota_fis;
+    double nota_geo;
+    double nota_quim;
+
+    public Aluno(String nome, int matricula, double nota_mat, double nota_por, double nota_fis, double nota_geo, double nota_quim)
+    {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota_mat = nota_mat;
+        this.nota_por = nota_por;
+        this.nota_fis = nota_fis;
+        this.nota_geo = nota_geo;
+        this.nota_quim = nota_quim;
+    }
+    public Double calcularMedia() {
+        return ((nota_mat + nota_por + nota_fis + nota_geo + nota_quim) / 5);
+    }
+    public String situacaoAluno() {
+        if (calcularMedia() >= 6) {
+            return "Você foi aprovado :)";
+        }
+        else{
+            return "Você foi reprovado :(";
+        }
+
+    }
+}
